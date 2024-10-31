d85dbb457fa53301ef467221b1c812d1bee1ed7f
OrangeJuiceFAST <165093244+OrangeJuiceFAST@users.noreply.github.com>
2024-10-31 10:33:33 -0500

    Add files via upload

---
+++ AggieBotClasses.cpp
@@ -0,0 +1,16 @@
+//test
+
+class Entity {
+    public:
+        int getXTile() {
+            return this->xTile;
+        };
+        int getYTile() {
+            return this->yTile;
+        };
+    private:
+        int xTile;
+        int yTile;   
+};
+
+class 
